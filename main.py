86b3b0037218300053a5819e4d1326ab5dad4a17
cacodcar <59067790+cacodcar@users.noreply.github.com>
2024-11-07 10:39:38 -0600

    requests pip

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#%% src/main.py
+# src/main.py
 from Frontend.gui import start_gui
 from Backend.logic import start_logic
+
 
 def main():
     # Inicializa la lógica del backend
     start_logic()
-    
+
     # Inicia la interfaz gráfica
     start_gui()
 
+
 if __name__ == "__main__":
     main()
-
-# %%
